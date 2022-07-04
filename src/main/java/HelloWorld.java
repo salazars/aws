3d47f0a4932bbd9d6fdf2d19cafe5eb769a91f56
magicalning <42858230+magicalning@users.noreply.github.com>
2022-07-04 14:38:25 +0800

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hell world local modify bbbd");
-        System.out.println("hell world local modify b and remote modify bd");
+        System.out.println("hell world local modify b and remote modify bdd");
     }
 }
